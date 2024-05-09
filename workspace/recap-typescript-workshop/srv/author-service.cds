3f424097683265dd794d7416572ded4c3654a0f0
Geert-Jan Klaps <geert-jan@klaps.org>
2024-05-09 16:41:51 +0200

    base application added action sample

--- workspace/recap-typescript-workshop/srv/author-service.cds
+++ workspace/recap-typescript-workshop/srv/author-service.cds
@@ -1,5 +1,12 @@
-using { sap.capire.bookshop as my } from '../db/schema';
+using {sap.capire.bookshop as my} from '../db/schema';
+
 service AuthorService {
-  @readonly entity Authors as projection on my.Authors;
-  @readonly entity Books as projection on my.Books;
+  @readonly
+  entity Authors as projection on my.Authors;
+
+  @readonly
+  entity Books   as projection on my.Books
+    actions {
+      action updateStock(quantity : Integer);
+    };
 }
--- workspace/recap-typescript-workshop/srv/author-service.js
+++ workspace/recap-typescript-workshop/srv/author-service.js
@@ -1,30 +1,42 @@
 const cds = require('@sap/cds/lib')
 
-module.exports = class AuthorService extends cds.ApplicationService { 
-  
-  init(){
+module.exports = class AuthorService extends cds.ApplicationService {
+
+  init() {
 
     this.after('READ', 'Authors', async (authors) => {
       // Ensure authors is an array (could be an object when selecting a single author)
       authors = Array.isArray(authors) ? authors : [authors];
-      
+
       // Put all author ID's for this request in an array for our query
       let authorIds = authors.map((author) => author.ID);
 
       // Get the total stock for each author in a single query to the database (code push down)
       let totalStock = await SELECT('SUM(stock) AS totalStock', 'author_ID').from('sap.capire.bookshop.Books').where({ author_ID: { 'in': authorIds } }).groupBy('author_ID');
-      
+
       // Enrich the resulting authors with total books in stock
-      authors = authors.map( (author) => {
+      authors = authors.map((author) => {
         const authorTotalStock = totalStock.find(ts => ts.author_ID === author.ID);
-        if(authorTotalStock){
+        if (authorTotalStock) {
           author.totalBooksInStock = authorTotalStock.totalStock;
         }
         return author;
       });
-      
+
       // Return final result
       return authors;
+    });
+
+    // Update stock for a book
+    this.on('updateStock', 'Books', async (req) => {
+      // Read the parameters from the request
+      const parameters = req.params[0];
+
+      // Update the stock for the given book
+      await UPDATE('sap.capire.bookshop.Books', parameters.ID).set(`stock = ${req.data.quantity}`);
+
+      // Notify the client that the stock has been updated
+      req.notify(200, 'Stock updated');
     });
 
     return super.init()
